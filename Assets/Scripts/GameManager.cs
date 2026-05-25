e2d87f763619fb156196a1fa747626cec7e1f67a
Vandal_67 <pedroapcabral@gmail.com>
2026-05-25 08:20:10 +0100

    70%

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public int fragmentosRecolhidos = 0;
     public int totalFragmentos = 4;
+    [SerializeField] private float timeRemaining = 60f;
+    private bool timerRunning = true;
+
+
 
     [SerializeField] private string nomeCenaSeguinte;
+
+    private String gameoverscene = "gameover"; //game over scene this can be serialized or public
+
+    [SerializeField] private HUDController hudController;
+
 
     public void RecolherFragmento()
     {
@@ -24,4 +35,34 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+
+    void Update()
+    {
+        if (timerRunning && timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else if (timeRemaining <= 0)
+        {
+            timeRemaining = 0;
+            timerRunning = false;
+            OnTimerEnd();
+        }
+
+
+        //regra de tres simples???
+
+        hudController.UpdateTemperature(timeRemaining);
+    }
+
+
+
+
+    void OnTimerEnd()
+    {
+        SceneManager.LoadScene(gameoverscene);
+    }
+        public void PauseTimer() => timerRunning = false;
+    public void ResumeTimer() => timerRunning = true;
+    public void ResetTimer() { timeRemaining = 60f; timerRunning = true; }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
 using UnityEngine;
 using TMPro;
+using System.Collections;
+using UnityEngine;
 
 
 
@@ -11,13 +12,23 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private Transform camera;
-
-    public TextMeshProUGUI Text;
+    [SerializeField]
+    private float forcaEmpurrao = 8f;
 
 
     float movementSpeed = 5;
-    float rotationspeed = 1000;
+    float rotationspeed = 500f;
 
+    private Vector3 movimento;
+
+
+
+    private float xRotation = 0f;
+    private float minX = -10f;  // how far up
+    private float maxX = 10f;
+
+
+    public Animator playerAnim;
 
     void Start()
     {
@@ -25,18 +36,17 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+
+    bool walking;
+
+
+
     void FixedUpdate()
     {
-
-
         //basic input
-        if (Input.GetKey("w"))
+        if (Input.GetKey("w")) //Keycode.w
         {
             transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
         }
         if (Input.GetKey("s"))
         {
@@ -46,46 +56,94 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
         }
-
-        //rotate player on x
-        //Yaw rotates the camera around its local Up axis
-        transform.Rotate(Vector3.up * Time.deltaTime * Input.GetAxis("Mouse X") * rotationspeed);
-
-        //this is for the camera
-        //Pitch rotates the camera around its local Right axis
-
-        // if ( camera.eulerAngles.y  > 300)
-        // {
-
-        // }
-        // else
-        // {
-        //     camera.eulerAngles = new Vector3(0, 90, 0); //this is not working
-        // }
-
-
-            camera.Rotate(Vector3.left * Time.deltaTime * Input.GetAxis("Mouse Y") * rotationspeed);
-        
-
-
-
-
-   
-            Text.text = camera.eulerAngles.y.ToString();
-        
+        if (Input.GetKey("a"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+        }
 
         //Sprint
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            movementSpeed = 20;
+            movementSpeed = 10;
         }
         else
         {
             movementSpeed = 5;
         }
+    }
 
 
 
+    void Update()
+    {
+
+        //aniamtion
+        if (Input.GetKeyDown("w")) //Keycode.w
+        {
+            playerAnim.SetTrigger("jog");
+            playerAnim.ResetTrigger("idle");
+            walking = true;
+
+        }
+        if (Input.GetKeyUp("w"))
+        {
+            playerAnim.ResetTrigger("jog");
+            playerAnim.SetTrigger("idle");
+            walking = false;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            playerAnim.SetTrigger("jogback");
+            playerAnim.ResetTrigger("idle");
+        }
+        if (Input.GetKeyUp("s"))
+        {
+            playerAnim.ResetTrigger("jogback");
+            playerAnim.SetTrigger("idle");
+        }
+
+
+        //rotate player on x
+        //Yaw rotates the camera around its local Up axis
+        transform.Rotate(Vector3.up * Time.deltaTime * Input.GetAxis("Mouse X") * rotationspeed);
+
+
+
+        float mouseY = Input.GetAxis("Mouse Y") * rotationspeed * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minX, maxX);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+
+        if (walking == true)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                playerAnim.SetTrigger("run");
+                playerAnim.ResetTrigger("jog");
+
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                playerAnim.SetTrigger("jog");
+                playerAnim.ResetTrigger("run");
+            }
+        }
+
+        //Sprint
+
+
+
+        //this needs better implementation
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        movimento = new Vector3(horizontal, 0f, vertical).normalized;
     }
 
     public void OnKey()
@@ -95,5 +153,26 @@ public class PlayerMovement : MonoBehaviour
 
 
 
+
+
+
+    //Colisions
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Empurravel"))
+        {
+            Rigidbody rbObjeto = collision.rigidbody;
+
+            if (rbObjeto != null && !rbObjeto.isKinematic)
+            {
+                Vector3 direcaoEmpurrao = new Vector3(movimento.x, 0f, movimento.z);
+
+                if (direcaoEmpurrao.magnitude > 0.1f)
+                {
+                    rbObjeto.AddForce(direcaoEmpurrao.normalized * forcaEmpurrao, ForceMode.Force);
+                }
+            }
+        }
+    }
 
 }
